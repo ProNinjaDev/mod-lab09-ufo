968a232f773a403e983072c3e6b6601c0a2e31b6
ProNinjaDev <pronin-04@bk.ru>
2025-05-25 11:51:08 +0300

    Add a button to start the experiment and update the motion simulation logic with the results saved to a CSV file

--- UFOMovement/Form1.cs
+++ UFOMovement/Form1.cs
@@ -1,6 +1,7 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Text;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace UFOMovement;
@@ -13,7 +14,9 @@ public partial class Form1 : Form
     private int objectRadius = 5;
 
     private PointF startPoint = new PointF(50, 50);
-    private PointF endPoint = new PointF(700, 400);
+    private PointF endPoint = new PointF(1500, 800);
+    private float targetZoneRadius = 5.0f;
+    private bool useScaling = true;
 
     private float k_slope;
     private float b_intercept;
@@ -26,6 +29,9 @@ public partial class Form1 : Form
 
     private System.Windows.Forms.Timer animationTimer = new System.Windows.Forms.Timer();
 
+    private Button runExperimentButton;
+    private Label statusLabel;
+
 
     public Form1()
     {
@@ -36,10 +42,118 @@ public partial class Form1 : Form
         animationTimer.Tick += AnimationTimer_Tick;
         animationTimer.Interval = 20;
 
-        InitializeMovementParameters();
+        runExperimentButton = new Button();
+        runExperimentButton.Text = "Run experiment";
+        runExperimentButton.AutoSize = true;
+        runExperimentButton.Location = new Point(100, 10);
+        runExperimentButton.Click += new EventHandler(RunExperimentButton_Click);
+        this.Controls.Add(runExperimentButton);
+
+        statusLabel = new Label();
+        statusLabel.Text = "...";
+        statusLabel.AutoSize = true;
+        statusLabel.Location = new Point(100, runExperimentButton.Bottom + 5);
+        this.Controls.Add(statusLabel);
+
+        currentX = startPoint.X;
+        currentY = startPoint.Y;
+
+        if (movementMethod == 1) {
+             targetAngle = Math.Atan2(endPoint.Y - startPoint.Y, endPoint.X - startPoint.X);
+        }
+
+        // InitializeMovementParameters();
     }
 
-    private void InitializeMovementParameters()
+    private async void RunExperimentButton_Click(object? sender, EventArgs e)
+    {
+        runExperimentButton.Enabled = false;
+        statusLabel.Text = "Experiment processing";
+        // Application.DoEvents();
+
+        PointF expStartPoint = this.startPoint;
+        PointF expEndPoint = this.endPoint;
+        float expMovementStep = this.movementStep;
+        List<float> radiiTest = new List<float> { 2.0f, 3.0f, 5.0f, 8.0f, 10.0f, 15.0f, 20.0f, 25.0f, 30.0f };
+        int minNTermsTest = 1;
+        int maxNTermsTest = 20;
+
+        List<string> csvLines = new List<string>();
+        csvLines.Add("Radius;NTerms");
+
+        await Task.Run(() =>
+        {
+            foreach (var currentTestRadius in radiiTest)
+            {
+                int foundNTerms = -1;
+
+                for (int n = minNTermsTest; n <= maxNTermsTest; n++)
+                {
+                    double actualFinalDistance;
+                    if (RunSilentSimulation(expStartPoint, expEndPoint,
+                                            expMovementStep, n,
+                                            currentTestRadius, out actualFinalDistance))
+                    {
+                        foundNTerms = n;
+                        break;
+                    }
+                }
+
+                csvLines.Add($"{currentTestRadius.ToString(CultureInfo.InvariantCulture)};{foundNTerms}");
+            }
+        });
+
+        string baseDirectory = AppContext.BaseDirectory;
+        string projectRoot = Path.GetFullPath(Path.Combine(baseDirectory, "..", "..", "..", ".."));
+        string pathToCsv = Path.Combine(projectRoot, "result");
+        string csvFilePath = Path.Combine(pathToCsv, "plot_data.csv");
+
+        File.WriteAllLines(csvFilePath, csvLines);
+
+        statusLabel.Text = "Experiment is over";
+        runExperimentButton.Enabled = true;
+        
+    }
+
+    private bool RunSilentSimulation(PointF simStartPoint, PointF simEndPoint,
+                                    float simMovementStep, int simNTerms,
+                                    float simTargetRadius, out double finalDistanceToTarget)
+    {
+        float currentSimX = simStartPoint.X;
+        float currentSimY = simStartPoint.Y;
+
+        double simTargetAngle = Math.Atan2(simEndPoint.Y - simStartPoint.Y, simEndPoint.X - simStartPoint.X);
+
+        int maxIterations = 2500;
+
+        for (int i = 0; i < maxIterations; i++)
+        {
+            double deltaX = simMovementStep * Cos(simTargetAngle, simNTerms);
+            double deltaY = simMovementStep * Sin(simTargetAngle, simNTerms);
+
+            currentSimX += (float)deltaX;
+            currentSimY += (float)deltaY;
+
+            finalDistanceToTarget = Math.Sqrt(Math.Pow(simEndPoint.X - currentSimX, 2) + Math.Pow(simEndPoint.Y - currentSimY, 2));
+
+            if (finalDistanceToTarget < simMovementStep)
+            {
+                return (finalDistanceToTarget <= simTargetRadius);
+            }
+
+            double initialSimDistance = Math.Sqrt(Math.Pow(simEndPoint.X - simStartPoint.X, 2) + Math.Pow(simEndPoint.Y - simStartPoint.Y, 2));
+            if (i > 10 && finalDistanceToTarget > initialSimDistance * 2 && initialSimDistance > 0) // TODO: зависимость от maxIterations
+            {
+                return false;
+            }
+        }
+
+        finalDistanceToTarget = Math.Sqrt(Math.Pow(simEndPoint.X - currentSimX, 2) + Math.Pow(simEndPoint.Y - currentSimY, 2));
+        return (finalDistanceToTarget <= simTargetRadius);
+    }
+    
+
+    private void InitializeSingleVisualRun()
     {
         currentX = startPoint.X;
         currentY = startPoint.Y;
@@ -60,6 +174,15 @@ public partial class Form1 : Form
     private void Form1_Paint(object? sender, PaintEventArgs e)
     {
         Graphics g = e.Graphics;
+        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+        if (useScaling)
+        {
+            g.ScaleTransform(0.5f, 0.5f);
+        }
+
+        g.DrawEllipse(Pens.LightGray, endPoint.X - targetZoneRadius, endPoint.Y - targetZoneRadius, 2 * targetZoneRadius, 2 * targetZoneRadius);
+
         g.FillEllipse(Brushes.Red, currentX - objectRadius, currentY - objectRadius, 2 * objectRadius, 2 * objectRadius);
 
         g.FillEllipse(Brushes.Green, startPoint.X - 5, startPoint.Y - 5, 10, 10);
@@ -85,7 +208,7 @@ public partial class Form1 : Form
         {
             return MoveByAngle();
         }
-        return false;
+        return true;
     }
 
     private bool MoveLine()
